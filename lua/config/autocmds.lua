ee8b9e880982ee7552f39a5dc73d3ce582d89501
reizzao <reizao1977@gmail.com>
2025-06-14 15:34:53 -0300

    Atualização automática - 2025-06-14 15:34:53

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,16 +8,3 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 
--- AREA TRANSFERENCIA NVIM >> LINUX --
-vim.cmd([[ set clipboard=unnamedplus ]])
--- compartilhar de área de transferência entre o Vim e o Linux
-
-
-
--- INICIAL -- Cursor DeOndeParou --
--- vim.cmd([[
---   hi! MatchParen cterm=NONE,bold gui=NONE,bold guibg=NONE guifg=#FFFF00
---   if has("autocmd")
---     au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
---   endif
--- ]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,11 @@
 -- Add any additional options here
 
 vim.g.guicursor = ""          -- depois $`reset` no terminal para assumir o cursor fininho : < IBeam > configurado no terminal.
-
 vim.g.mapleader = " "         -- Define a tecla 'leader' (geralmente configurado em um local anterior, como init.lua do LazyVim)
+
+-- AREA TRANSFERENCIA NVIM >> LINUX --
+vim.cmd([[ set clipboard=unnamedplus ]])   -- compartilhar de área de transferência entre o Vim e o Linux
+
+
+
+-

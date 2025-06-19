a5e65e68f8ef0ea7f089fa8abe8f838c37816996
reizzao <reizao1977@gmail.com>
2025-06-19 03:06:49 -0300

    telescope - funcao duplo click term

---
+++ lua/custom/options.lua
@@ -0,0 +1,4 @@
+-- ~/config/nvim/lua/custom/options.lua
+
+vim.opt.mouse = "a" -- fn handle_terminal_click() - depende desta opção
+vim.opt.swapfile = false -- não vai mais recuperar arquivos perdidos swa
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,23 @@
--- plugins/telescope
+-- ~/.config/nvim/lua/plugins/telescope.lua
 
 return {
-  -- Telescope (para busca e navegação)
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x", -- ou master, dependendo da sua preferência
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" }) -- Pesquisa em tempo real com ripgrep
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
-    end,
+  -- ...
+  opts = {
+    pickers = {
+      find_files = {
+        hidden = false,
+        no_ignore = false, -- MUITO IMPORTANTE: Garante que .gitignore e .rgignore/config sejam respeitados
+      },
+      live_grep = {
+        hidden = false,
+        no_ignore = false, -- MUITO IMPORTANTE
+      },
+      live_grep_args = {
+        hidden = false,
+        no_ignore = false, -- MUITO IMPORTANTE
+      },
+    },
+    -- ...
   },
+  -- ...
 }

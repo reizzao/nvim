4135e5ddc67953f806cdeb6502f0127893923f70
reizzao <reizao1977@gmail.com>
2025-06-20 01:05:38 -0300

    feat: config telescope ok

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,22 @@
--- ~/.config/nvim/lua/plugins/telescope.lua
+-- ~/.config/nvim/lua/plugins/telescope.lua (ou o arquivo onde você customiza o Telescope)
 
 return {
-  -- ...
+  "nvim-telescope/telescope.nvim",
   opts = {
+    defaults = {
+      -- Opções padrão para todos os pickers
+      hidden = false,    -- Não mostra arquivos/diretórios ocultos por padrão
+      no_ignore = false, -- Respeita .gitignore por padrão
+    },
     pickers = {
+      -- Você pode sobrescrever aqui para pickers específicos se quiser
       find_files = {
-        hidden = false,
-        no_ignore = false, -- MUITO IMPORTANTE: Garante que .gitignore e .rgignore/config sejam respeitados
+        -- hidden = true, -- Exemplo: se quisesse mostrar ocultos SÓ para find_files
       },
       live_grep = {
-        hidden = false,
-        no_ignore = false, -- MUITO IMPORTANTE
-      },
-      live_grep_args = {
-        hidden = false,
-        no_ignore = false, -- MUITO IMPORTANTE
+        -- hidden = true, -- Exemplo: se quisesse mostrar ocultos SÓ para live_grep
       },
     },
-    -- ...
+    -- ... outras opções do Telescope ...
   },
-  -- ...
 }

4812a0007c79650fe9f861ce5e2fb8d5f52bc3b3
reizzao <reizao1977@gmail.com>
2025-07-05 15:15:18 -0300

    ok nvim pure

--- init.lua
+++ init.lua
@@ -5,13 +5,13 @@ local loader = require('custom.utils.loader')
 
 -- --- Configurações Core ---
 -- Certifique-se de que 'termguicolors' esteja true aqui para que as cores funcionem
-loader.load_directory('core')
+loader.load_directory('custom.core')
 
 -- --- Def Tema Padrao (Colorscheme) ---
 -- Para um tema do GitHub (ex: catppuccin): -- Certifique-se de ter o plugin instalado primeiro! -Obs: este nao precisa de estar compativel na pasta colors/
-vim.cmd("colorscheme catppuccin")  " [ON]]
--- vim.cmd("colorscheme theme_tokyo") " [OFF : TODO]]
--- vim.cmd("colorscheme meu_tema") " [OFF : TODO]]
+vim.cmd("colorscheme catppuccin")
+-- vim.cmd("colorscheme theme_tokyo")
+-- vim.cmd("colorscheme meu_tema")
 
 
 -- vim.cmd("colorscheme catppuccin")
